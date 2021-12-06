ac8c119c385dee96c9f12a655f392bc8e46e9ded
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-12-05 22:28:09 -0600

    component skeletons

---
+++ src/Box.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Box = ({ backgroundColor, width, height }) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Box;
---
+++ src/BoxList.js
@@ -0,0 +1,10 @@
+import React from "react";
+import NewBoxForm from './NewBoxForm';
+
+const BoxList = () => {
+    return (
+
+    )
+}
+
+export default BoxList;
