ccdd2e58807ef583b72a55c2fad9f967c0e81035
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-12-05 22:44:10 -0600

    delete button

--- src/Box.js
+++ src/Box.js
@@ -1,8 +1,14 @@
 import React from "react";
 
 const Box = ({ backgroundColor, width, height }) => {
+    const handleClick = () => {
+        
+    }
+
     return (
-        <div backgroundColor={backgroundColor} width={width} height={height}></div>
+        <div backgroundColor={backgroundColor} width={width} height={height}>
+            <button onClick={handleClick}>X</button>
+        </div>
     )
 }
 
