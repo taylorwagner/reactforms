fca3f3e441a226fa148712838dd3df18c408fe59
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-12-05 22:59:19 -0600

    add divs in for each input

--- src/NewBoxForm.js
+++ src/NewBoxForm.js
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
-import Box from './Box';
+import uuid from "uuid/v4";
 
 const NewBoxForm = () => {
     const initialState = {
         backgroundColor: "",
         width: "",
         height: ""
-    }
+    };
 
     const [formData, setFormData] = useState(initialState);
 
@@ -15,49 +15,53 @@ const NewBoxForm = () => {
         setFormData(data => ({
             ...data,
             [name]: value
-        }))
-    }
+        }));
+    };
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        const { backgroundColor, width, height } = formData;
+        createBox({ ...formData, id: uuid() });
         setFormData(initialState);
-    }
+    };
 
     return (
         <>
             <form onSubmit={handleSubmit}>
-                <label htmlFor="backgroundColor">Background Color of Box</label>
-                <input
-                    id="backgroundColor"
-                    type="text"
-                    name="backgroundColor"
-                    placeholder="background color of box"
-                    value={formData.backgroundColor}
-                    onChange={handleChange}
-                />
+                <div>
+                    <label htmlFor="backgroundColor">Background Color of Box</label>
+                    <input
+                        id="backgroundColor"
+                        type="text"
+                        name="backgroundColor"
+                        placeholder="background color of box"
+                        value={formData.backgroundColor}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="width">Width of Box</label>
+                    <input
+                        type="text"
+                        placeholder="width of box"
+                        name="width"
+                        id="width"
+                        value={formData.width}
+                        onChange={handleChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="height">Height of Box</label>
+                    <input
+                        type="text"
+                        placeholder="height of box"
+                        name="height"
+                        id="height"
+                        value={formData.height}
+                        onChange={handleChange}
+                    />
+                </div>
 
-                <label htmlFor="width">Width of Box</label>
-                <input
-                    type="text"
-                    placeholder="width of box"
-                    name="width"
-                    id="width"
-                    value={formData.width}
-                    onChange={handleChange}
-                />
-
-                <label htmlFor="height">Height of Box</label>
-                <input
-                    type="text"
-                    placeholder="height of box"
-                    name="height"
-                    id="height"
-                    value={formData.height}
-                    onChange={handleChange}
-                />
-
-                <button>Create desired box!</button>
+                <button id="newBoxButton">Create desired box!</button>
             </form>
         </>
     )
