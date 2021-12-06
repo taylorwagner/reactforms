73f9575450e3b96378def099a6ce01d496107327
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-12-05 22:53:49 -0600

    many boxes - map

--- src/BoxList.js
+++ src/BoxList.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Box from './Box';
 import NewBoxForm from './NewBoxForm';
 
 const BoxList = () => {
+    const [boxes, setNewBoxes] = useState([]);
+    const add = boxObj => {
+        setNewBoxes(boxes => [...boxes, boxObj]);
+    };
+    const remove = id => {
+        setNewBoxes(boxes => boxes.filter(box => box.id !== id));
+    };
+
+    const boxComp = boxes.map(box => (
+        <Box
+            key={box.id}
+            id={box.id}
+            width={box.width}
+            height={box.height}
+            handleRemove={remove}
+            backgroundColor={box.backgroundColor}
+        />
+    ));
+
     return (
         <>
-            <Box />
-            <NewBoxForm />
+            <NewBoxForm createBox={add} />
+            {boxComp}
         </>
     );
 }
