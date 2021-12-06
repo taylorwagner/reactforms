a78678c731c56ec4124636a3778252854d4c3097
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-12-05 22:30:02 -0600

    rendering other components

--- src/BoxList.js
+++ src/BoxList.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Box from './Box';
 import NewBoxForm from './NewBoxForm';
 
 const BoxList = () => {
     return (
-
-    )
+        <>
+            <Box />
+            <NewBoxForm />
+        </>
+    );
 }
 
 export default BoxList;
